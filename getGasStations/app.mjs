9cdc1e9a8722a50ececb58f2fc15971888e5a3c0
YeranConcepcion <yeran.concepcion@upr.edu>
2024-10-21 22:34:26 -0400

    created new serverless backend using dynamo

---
+++ getGasStations/app.mjs
@@ -0,0 +1,46 @@
+import {DynamoDBClient} from "@aws-sdk/client-dynamodb"
+import {DynamoDBDocumentClient,ScanCommand} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({})
+const docClient = DynamoDBDocumentClient.from(client)
+var table = process.env.TABLE_GAS_STATIONS
+export const lambdaHandler = async (event, context) => {
+  var tableData = []
+
+  var parms = {
+    ProjectionExpression: "Station_ID",
+    TableName: table,
+  }
+  try{
+    const command = new ScanCommand(parms)
+    var response = await docClient.send(command)
+    tableData = response.Items
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods': 'OPTIONS,GET'
+        
+      },
+      body: JSON.stringify(JSON.stringify(tableData))
+    };
+
+    return response;
+    
+
+  }
+  catch(err){
+    const errResponse = {
+      statusCode: 400,
+      body: "Failed to retreive table data from Gas Station table, Error: "+err
+    };
+
+    return errResponse;
+
+  }
+
+   
+  };
+  
